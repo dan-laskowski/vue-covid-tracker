8422f6165b03e4777339bd8b25da593fb6c384ab
dan-laskowski <daniel.laskowski94@gmail.com>
2021-11-18 13:51:44 +0100

    bind data from api to select html element

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>Select</div>
-  <select name="Countries" id="countries">
-    <option v-for="(country, index) in countries" :key="index" :value="item">
-      {{ country.Slug }}
-    </option>
-  </select>
+  <div>
+    <select
+      v-model="selected"
+      @change="onChange()"
+      name="Countries"
+      id="countries"
+      class="form-select mt-10 block w-full border p-3 rounded"
+    >
+      <option value="0" disabled>Select Country</option>
+      <option
+        v-for="country in countries"
+        :key="country.ID"
+        :value="country.ID"
+      >
+        {{ country.Country }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'CountrySelect',
   props: ['countries'],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+      console.log(country);
+      this.$emit('get-country');
+    },
+  },
 };
 </script>
